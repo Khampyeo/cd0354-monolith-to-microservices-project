fffe8d139790e60451e769d8cea2b8a76f28dbb0
ckiet1201 <ckiet1201@gmail.com>
2024-10-30 10:01:12 +0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "http://reverseproxy-service:80//api/v0",
+  apiHost: "http://reverseproxy-service:80/api/v0",
 };
 
 /*
